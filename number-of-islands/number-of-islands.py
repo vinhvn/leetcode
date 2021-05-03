5f704250e62fdda1c807c06a66eda3e7c896965d
Vincent (Vinh) Nguyen <47393759+vinhvn@users.noreply.github.com>
2021-05-03 11:26:23 -0400

    Time: 160 ms (22.89%), Space: 15.4 MB (70.00%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,37 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        def findIsland():
-            for y in range(len(grid)):
-                for x in range(len(grid[0])):
-                    if grid[y][x] == "1":
-                        return (True, x, y)
-            return (False,-1,-1)
+        res = 0
+        m, n = (len(grid), len(grid[0]))
         
-        def destroy(x, y):
-            # if already visited, return
-            if grid[y][x] == "0":
-                return
-            
-            # set to visited
+        # helper to check bounds
+        def inBounds(x, y):
+            return (x >= 0 and x < n and y >= 0 and y < m)
+        
+        # helper to get adjacent grid slots
+        def getAdj(x, y):
+            d = [(1,0), (-1,0), (0,1), (0,-1)]
+            return [(x+i, y+j) for (i,j) in d if inBounds(x+i, y+j)]
+        
+        def dfs(x, y):
             grid[y][x] = "0"
-            
-            # bounds check and recurse
-            if (x - 1 >= 0):
-                destroy(x-1, y)
-            if (x + 1 < len(grid[0])):
-                destroy(x+1, y)
-            if (y - 1 >= 0):
-                destroy(x, y-1)
-            if (y + 1 < len(grid)):
-                destroy(x, y+1)
-            
-        (found, x, y) = findIsland()
-        numIslands = 0
+            for (i,j) in getAdj(x,y):
+                if grid[j][i] == "1":
+                    dfs(i,j)
         
-        while found:
-            numIslands += 1
-            destroy(x,y)
-            (found, x, y) = findIsland()
+        for j in range(m):
+            for i in range(n):
+                if grid[j][i] == "1":
+                    res += 1
+                    dfs(i,j)
         
-        return numIslands
+        return res
