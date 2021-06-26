c1320746409ecc2289b7f997fcef89e92585938b
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2021-06-26 17:40:28 -0400

    Time: 56 ms (66.17%), Space: 15.7 MB (6.66%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # dict to store complement (key) and index 
         d = {}
+        
         for i in range(len(nums)):
             n = nums[i]
+            # if number is in dict, we found a pair
             if n in d:
                 return [d[n], i]
             else:
                 d[target - n] = i
+        
+        # Time complexity: O(n)
+        #   The algorithm will loop for n iterations at most once
+        # Space complexity: O(n)
+        #   This algorithm uses a hashmap which can grow up to n keys if no suitable complements can be found
