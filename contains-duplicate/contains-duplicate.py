660825ddf08b75a0bbae9e2d1884c1dc3da4b131
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2021-07-01 15:43:52 -0400

    Time: 104 ms (99.39%), Space: 20 MB (82.05%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -1,5 +1,14 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set(nums)
+        """
+        create a set containing all elements of nums
+        compare the length of the set against the length of the list
+        if equal, the list contains no duplicates
+        """
+        return len(set(nums)) != len(nums)
         
-        return True if len(s) - len(nums) != 0 else False
+        """
+        n = len(nums)
+        Time complexity: O(n), linear time. Creating a set of all elements of nums will iterate over each element of nums at least once.
+        Space complexity: O(n), linear space. The set will contain all elements of nums at most.
+        """
