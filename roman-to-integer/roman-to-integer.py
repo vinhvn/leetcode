2f9f3b162b2b12f930848edfdb3b52f0e1fc3c9d
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2021-06-30 21:37:31 -0400

    Time: 52 ms (44.84%), Space: 14.2 MB (56.37%) - LeetHub

---
+++ roman-to-integer/roman-to-integer.py
@@ -0,0 +1,38 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        """
+        create a hashmap of symbol and values
+        iterate through input string
+        add values according to map
+        if instances of subtraction exist, check for them
+        """
+        
+        hashmap = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+            "IV": 4,
+            "IX": 9,
+            "XL": 40,
+            "XC": 90,
+            "CD": 400,
+            "CM": 900,
+        }
+        
+        out = 0
+        i = 0
+        while (i < len(s)):
+            val = hashmap[s[i]]
+            
+            if (i != len(s) - 1 and s[i:i+2] in hashmap):
+                val = hashmap[s[i:i+2]]
+                i += 1
+            
+            out += val
+            i += 1
+        
+        return out
