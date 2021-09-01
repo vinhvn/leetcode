dbbe837da932df7c6f3d40aa774277204c8e6748
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2021-09-01 00:38:46 -0400

    Time: 193 ms (14.28%), Space: 15.4 MB (72.75%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,28 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [(-1,0), (0,1), (1,0), (0,-1)]
+        m = len(grid)
+        n = len(grid[0])
+        islands = 0
         
-        res = 0
-        m, n = (len(grid), len(grid[0]))
-        
-        # helper to check bounds
-        def inBounds(x, y):
-            return (x >= 0 and x < n and y >= 0 and y < m)
-        
-        # helper to get adjacent grid slots
-        def getAdj(x, y):
-            d = [(1,0), (-1,0), (0,1), (0,-1)]
-            return [(x+i, y+j) for (i,j) in d if inBounds(x+i, y+j)]
-        
-        def dfs(x, y):
+        def dfs(y, x):
+            if (y < 0 or y >= m) or (x < 0 or x >= n) or grid[y][x] == "0":
+                return
             grid[y][x] = "0"
-            for (i,j) in getAdj(x,y):
-                if grid[j][i] == "1":
-                    dfs(i,j)
+            for dy, dx in directions:
+                dfs(y+dy, x+dx)
         
-        for j in range(m):
-            for i in range(n):
-                if grid[j][i] == "1":
-                    res += 1
-                    dfs(i,j)
+        for y in range(m):
+            for x in range(n):
+                if grid[y][x] == "1":
+                    islands += 1
+                    dfs(y, x)
         
-        return res
+        return islands
+            
