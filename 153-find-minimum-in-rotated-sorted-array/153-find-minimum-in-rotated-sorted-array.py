9cd039ed5c3a0e001273602cc6c4c0651339c34a
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2022-02-25 20:47:24 -0500

    Time: 40 ms (90.58%), Space: 14.4 MB (45.02%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -2,20 +2,24 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         n = len(nums)
         
+        # modified binary search to find point of rotation
         def binary_search(l, r):
             if l > r:
                 return -1
-            
+            # midpoint
             mid = (l + r) // 2
-            
+            # return point of rotation if found
             if nums[mid] < nums[mid-1]:
                 return nums[mid]
             elif nums[mid+1] < nums[mid]:
                 return nums[mid+1]
+            # otherwise, recurse on half
             elif nums[mid] > nums[l]:
                 return binary_search(mid, r)
             return binary_search(l, mid)
         
+        # if only 1 element or already sorted, return min
         if n == 1 or nums[n-1] > nums[0]:
             return nums[0]
+        # else, run binary search
         return binary_search(0, n-1)
