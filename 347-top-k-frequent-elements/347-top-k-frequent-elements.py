33ac9ab080d591d14668e3985d59dc386f67592f
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2022-03-02 15:59:48 -0500

    Time: 139 ms (56.78%), Space: 18.7 MB (75.31%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -5,11 +5,17 @@ class Solution:
         # build hashmap of num-frequency pairs
         num_freq = {}
         for num in nums:
-            num_freq[num] = num_freq.get(num, 0) - 1
-        # add all to heap
+            num_freq[num] = num_freq.get(num, 0) + 1
+        # add all to heap, ensuring only k exist at a time
         heap = []
+        i = 0
         for key, val in num_freq.items():
-            heapq.heappush(heap, (val, key))
+            if i < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+            i += 1
+                
         output = []
         # pop max k
         for i in range(k):
