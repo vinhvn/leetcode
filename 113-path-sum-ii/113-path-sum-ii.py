98b64667ccc05d2eb7f24428c65fe7f8ed77b210
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2022-03-09 12:03:46 -0500

    Time: 73 ms (35.43%), Space: 19.7 MB (8.26%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        paths = []
+        def dfs(node, curSum, curPath):
+            if not node:
+                return
+            curSum += node.val
+            path = curPath.copy()
+            path.append(node.val)
+            if curSum == targetSum and not node.left and not node.right:
+                paths.append(path)
+                return
+            
+            dfs(node.left, curSum, path)
+            dfs(node.right, curSum, path)
+        
+        dfs(root, 0, [])
+        return paths
