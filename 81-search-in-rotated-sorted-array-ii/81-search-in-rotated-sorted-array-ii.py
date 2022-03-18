c898335a05c57ad2702dca96521946d758fb2267
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2022-03-18 01:27:31 -0400

    Time: 94 ms (25.11%), Space: 14.5 MB (39.40%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -5,7 +5,6 @@ class Solution:
         start, end = 0, len(nums) - 1
         while start <= end:
             mid = start + (end - start) // 2
-            print(start, nums[start], nums[mid], nums[end], end)
             if key == nums[mid]:
                 return True
             # shrink if all indices result in the same number
