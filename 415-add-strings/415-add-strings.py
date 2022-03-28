7cfef9e7ae9faf1767f8b928b4b82cd02584500f
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2022-03-28 19:25:02 -0400

    Time: 28 ms (99.57%), Space: 14.5 MB (10.48%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -1,30 +1,18 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        def getInt(n):
-            return ord(n) - 48
-
+        nums1 = list(num1)
+        nums2 = list(num2)
         carry = 0
-        ans = []
-        p1, p2 = len(num1) - 1, len(num2) - 1
-        while p1 >= 0 and p2 >= 0:
-            s = getInt(num1[p1]) + getInt(num2[p2]) + carry
-            digit = s % 10
-            carry = s // 10
-            ans.append(str(digit))
-            p1 -= 1
-            p2 -= 1
-        while p1 >= 0:
-            s = getInt(num1[p1]) + carry
-            digit = s % 10
-            carry = s // 10
-            ans.append(str(digit))
-            p1 -= 1
-        while p2 >= 0:
-            s = getInt(num2[p2]) + carry
-            digit = s % 10
-            carry = s // 10
-            ans.append(str(digit))
-            p2 -= 1
+        res = []
+
+        while nums1 or nums2:
+            n1 = n2 = 0
+            if nums1:
+                n1 = ord(nums1.pop()) - ord("0")
+            if nums2:
+                n2 = ord(nums2.pop()) - ord("0")
+            carry, remainder = divmod(n1 + n2 + carry, 10)
+            res.append(str(remainder))
         if carry:
-            ans.append(str(carry))
-        return "".join(ans[::-1])
+            res.append(str(carry))
+        return "".join(res[::-1])
