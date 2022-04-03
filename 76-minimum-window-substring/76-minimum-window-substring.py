b348864ae79601ebed8759957164c9f8b4dfc031
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2022-04-03 16:06:45 -0400

    Time: 170 ms (45.83%), Space: 14.7 MB (39.36%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,27 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        char_freq = {}
-        min_len, min_start, matches, start = len(s) + 1, 0, 0, 0
-        for c in t:
-            if c not in char_freq:
-                char_freq[c] = 0
-            char_freq[c] += 1
+        patternFreq = defaultdict(int)
+        for char in t:
+            patternFreq[char] += 1
+        matches = 0
+        minStart, minEnd = 0, math.inf
+        # sliding window
+        start = 0
         for end in range(len(s)):
-            end_char = s[end]
-            if end_char in char_freq:
-                char_freq[end_char] -= 1
-                if char_freq[end_char] == 0:
+            if s[end] in patternFreq:
+                patternFreq[s[end]] -= 1
+                if patternFreq[s[end]] == 0:
                     matches += 1
-            while matches == len(char_freq):
-                if min_len > end - start + 1:
-                    min_len = end - start + 1
-                    min_start = start
-                start_char = s[start]
-                start += 1
-                if start_char in char_freq:
-                    if char_freq[start_char] == 0:
+            
+            while start <= end and matches == len(patternFreq):
+                # update min
+                if minEnd - minStart > end - start:
+                    minStart, minEnd = start, end
+                if s[start] in patternFreq:
+                    if patternFreq[s[start]] == 0:
                         matches -= 1
-                    char_freq[start_char] += 1
-        if min_len == len(s) + 1:
+                    patternFreq[s[start]] += 1
+                start += 1
+        
+        if minEnd == math.inf:
             return ""
-        return s[min_start : min_start + min_len]
+        return s[minStart : minEnd+1]
