e28ed30a4809a45fe10d39ef6fda626d2d2e6765
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2022-04-04 17:25:53 -0400

    Time: 77 ms (16.60%), Space: 14.3 MB (27.65%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -1,25 +1,24 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        n = len(nums)
-        
-        # modified binary search to find point of rotation
-        def binary_search(l, r):
-            if l > r:
-                return -1
-            # midpoint
-            mid = (l + r) // 2
-            # return point of rotation if found
-            if nums[mid] < nums[mid-1]:
-                return nums[mid]
-            elif nums[mid+1] < nums[mid]:
-                return nums[mid+1]
-            # otherwise, recurse on half
-            elif nums[mid] > nums[l]:
-                return binary_search(mid, r)
-            return binary_search(l, mid)
-        
-        # if only 1 element or already sorted, return min
-        if n == 1 or nums[n-1] > nums[0]:
+        # base cases
+        # 1 elem array or arr in sorted order (no rotations)
+        if len(nums) == 1 or nums[0] < nums[-1]:
             return nums[0]
-        # else, run binary search
-        return binary_search(0, n-1)
+        
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            # return point of rotation if found
+            if nums[mid-1] > nums[mid]:
+                return nums[mid]
+            elif nums[mid] > nums[mid+1]:
+                return nums[mid+1]
+
+            # otherwise, do some binary search
+            if nums[mid] > nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
