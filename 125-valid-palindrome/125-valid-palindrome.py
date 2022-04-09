5ebe7f315f63ffbed9776df825152341e7f1427d
Vincent Nguyen <47393759+vinhvn@users.noreply.github.com>
2022-04-08 20:50:05 -0400

    Time: 52 ms (80.37%), Space: 14.3 MB (88.21%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -7,12 +7,11 @@ class Solution:
         while left <= right:
             if not s[left].isalnum():
                 left += 1
-                continue
             elif not s[right].isalnum():
                 right -= 1
-                continue
             elif s[left].lower() != s[right].lower():
                 return False
-            left += 1
-            right -= 1
+            else:
+                left += 1
+                right -= 1
         return True
